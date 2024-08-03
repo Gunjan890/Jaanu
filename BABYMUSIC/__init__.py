dcd0a44c6dad20f690cf77c041b4c9f2c26e3f9a
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-04 00:26:15 +0530

    Add files via upload

---
+++ BABYMUSIC/__init__.py
@@ -0,0 +1,26 @@
+from BABYMUSIC.core.bot import Baby
+from BABYMUSIC.core.dir import dirr
+from BABYMUSIC.core.git import git
+from BABYMUSIC.core.userbot import Userbot
+from BABYMUSIC.misc import dbb, heroku
+
+from .logging import LOGGER
+
+dirr()
+git()
+dbb()
+heroku()
+
+app = Baby()
+userbot = Userbot()
+
+
+from .platforms import *
+
+Apple = AppleAPI()
+Carbon = CarbonAPI()
+SoundCloud = SoundAPI()
+Spotify = SpotifyAPI()
+Resso = RessoAPI()
+Telegram = TeleAPI()
+YouTube = YouTubeAPI()
