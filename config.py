8456c85a86992d9a0a2ac07b38638268dc77f87c
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-09 00:36:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/Gunjan890/DISHAQUEEN", # dont Change this otherwise u get error 🧧
+    "UPSTREAM_REPO", "https://github.com/Gunjan890/jaanu", # dont Change this otherwise u get error 🧧
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
