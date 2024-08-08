f7894b59c0c8e9ebf63a0d65fb0d2cf58b8ac77d
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-08 23:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Gunjan890/PURVI-MUSIC",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
