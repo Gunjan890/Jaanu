0be90296874eeff6a3ba19096f66798317bb7e67
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-17 23:19:26 +0530

    Update callback.py

--- BABYMUSIC/plugins/admins/callback.py
+++ BABYMUSIC/plugins/admins/callback.py
@@ -1,10 +1,11 @@
+arkup_timer())
 import asyncio
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from BABYMUSIC import YouTube, app, YTB
-from BABYMUSIC.core.call import baby
+from BABYMUSIC import YouTube, app
+from BABYMUSIC.core.call import Spotify
 from BABYMUSIC.misc import SUDOERS, db
 from BABYMUSIC.utils.database import (
     get_active_chats,
@@ -22,9 +23,9 @@ from BABYMUSIC.utils.formatters import seconds_to_min
 from BABYMUSIC.utils.inline import (
     close_markup,
     stream_markup,
+    stream_markup2, 
     stream_markup_timer,
-    telegram_markup,
-    telegram_markup_timer,
+    stream_markup_timer2,
 )
 from BABYMUSIC.utils.stream.autoclear import auto_clean
 from BABYMUSIC.utils.thumbnails import get_thumb
@@ -141,26 +142,27 @@ async def del_back_playlist(client, CallbackQuery, _):
             return await CallbackQuery.answer(_["admin_1"], show_alert=True)
         await CallbackQuery.answer()
         await music_off(chat_id)
-        await baby.pause_stream(chat_id)
+        await Spotify.pause_stream(chat_id)
         await CallbackQuery.message.reply_text(
-            _["admin_2"].format(mention),
+            _["admin_2"].format(mention), reply_markup=close_markup(_)
         )
     elif command == "Resume":
         if await is_music_playing(chat_id):
             return await CallbackQuery.answer(_["admin_3"], show_alert=True)
         await CallbackQuery.answer()
         await music_on(chat_id)
-        await baby.resume_stream(chat_id)
+        await Spotify.resume_stream(chat_id)
         await CallbackQuery.message.reply_text(
-            _["admin_4"].format(mention),
+            _["admin_4"].format(mention), reply_markup=close_markup(_)
         )
     elif command == "Stop" or command == "End":
         await CallbackQuery.answer()
-        await baby.stop_stream(chat_id)
+        await Spotify.stop_stream(chat_id)
         await set_loop(chat_id, 0)
         await CallbackQuery.message.reply_text(
-            _["admin_5"].format(mention),
+            _["admin_5"].format(mention), reply_markup=close_markup(_)
         )
+        await CallbackQuery.message.delete()
     elif command == "Skip" or command == "Replay":
         check = db.get(chat_id)
         if command == "Skip":
@@ -178,10 +180,10 @@ async def del_back_playlist(client, CallbackQuery, _):
                         text=_["admin_6"].format(
                             mention, CallbackQuery.message.chat.title
                         ),
-                        
+                        reply_markup=close_markup(_),
                     )
                     try:
-                        return await baby.stop_stream(chat_id)
+                        return await Spotify.stop_stream(chat_id)
                     except:
                         return
             except:
@@ -193,8 +195,9 @@ async def del_back_playlist(client, CallbackQuery, _):
                         text=_["admin_6"].format(
                             mention, CallbackQuery.message.chat.title
                         ),
+                        reply_markup=close_markup(_),
                     )
-                    return await baby.stop_stream(chat_id)
+                    return await Spotify.stop_stream(chat_id)
                 except:
                     return
         else:
@@ -219,17 +222,17 @@ async def del_back_playlist(client, CallbackQuery, _):
             if n == 0:
                 return await CallbackQuery.message.reply_text(
                     text=_["admin_7"].format(title),
-                    
+                    reply_markup=close_markup(_),
                 )
             try:
                 image = await YouTube.thumbnail(videoid, True)
             except:
                 image = None
             try:
-                await VIP.skip_stream(chat_id, link, video=status, image=image)
+                await Spotify.skip_stream(chat_id, link, video=status, image=image)
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
-            button = telegram_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             img = await get_thumb(videoid)
             run = await CallbackQuery.message.reply_photo(
                 photo=img,
@@ -256,21 +259,13 @@ async def del_back_playlist(client, CallbackQuery, _):
                     video=status,
                 )
             except:
-                try:
-                    file_path, direct = await YTB.download(
-                        videoid,
-                        mystic,
-                        videoid=True,
-                        video=status,
-                    )
-                except:
-                    return await mystic.edit_text(_["call_6"])
+                return await mystic.edit_text(_["call_6"])
             try:
                 image = await YouTube.thumbnail(videoid, True)
             except:
                 image = None
             try:
-                await VIP.skip_stream(chat_id, file_path, video=status, image=image)
+                await Spotify.skip_stream(chat_id, file_path, video=status, image=image)
             except:
                 return await mystic.edit_text(_["call_6"])
             button = stream_markup(_, videoid, chat_id)
@@ -291,10 +286,10 @@ async def del_back_playlist(client, CallbackQuery, _):
             await mystic.delete()
         elif "index_" in queued:
             try:
-                await baby.skip_stream(chat_id, videoid, video=status)
+                await Spotify.skip_stream(chat_id, videoid, video=status)
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
-            button = telegram_markup(_, chat_id)
+            button = stream_markup2(_, chat_id)
             run = await CallbackQuery.message.reply_photo(
                 photo=STREAM_IMG_URL,
                 caption=_["stream_2"].format(user),
@@ -314,17 +309,15 @@ async def del_back_playlist(client, CallbackQuery, _):
                 except:
                     image = None
             try:
-                await baby.skip_stream(chat_id, queued, video=status, image=image)
+                await Spotify.skip_stream(chat_id, queued, video=status, image=image)
             except:
                 return await CallbackQuery.message.reply_text(_["call_6"])
             if videoid == "telegram":
-                button = telegram_markup(_, chat_id)
+                button = stream_markup2(_, chat_id)
                 run = await CallbackQuery.message.reply_photo(
-                    photo=(
-                        TELEGRAM_AUDIO_URL
-                        if str(streamtype) == "audio"
-                        else TELEGRAM_VIDEO_URL
-                    ),
+                    photo=TELEGRAM_AUDIO_URL
+                    if str(streamtype) == "audio"
+                    else TELEGRAM_VIDEO_URL,
                     caption=_["stream_1"].format(
                         config.SUPPORT_CHAT, title[:23], duration, user
                     ),
@@ -333,13 +326,11 @@ async def del_back_playlist(client, CallbackQuery, _):
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "tg"
             elif videoid == "soundcloud":
-                button = telegram_markup(_, chat_id)
+                button = stream_markup2(_, chat_id)
                 run = await CallbackQuery.message.reply_photo(
-                    photo=(
-                        SOUNCLOUD_IMG_URL
-                        if str(streamtype) == "audio"
-                        else TELEGRAM_VIDEO_URL
-                    ),
+                    photo=SOUNCLOUD_IMG_URL
+                    if str(streamtype) == "audio"
+                    else TELEGRAM_VIDEO_URL,
                     caption=_["stream_1"].format(
                         config.SUPPORT_CHAT, title[:23], duration, user
                     ),
@@ -366,7 +357,7 @@ async def del_back_playlist(client, CallbackQuery, _):
 
 
 async def markup_timer():
-    while not await asyncio.sleep(4):
+    while not await asyncio.sleep(1):
         active_chats = await get_active_chats()
         for chat_id in active_chats:
             try:
@@ -378,6 +369,37 @@ async def markup_timer():
                 duration_seconds = int(playing[0]["seconds"])
                 if duration_seconds == 0:
                     continue
+                try:
+                    mystic = playing[0]["markup"]
+                except:
+                    continue
+                try:
+                    check = checker[chat_id][mystic.id]
+                    if check is False:
+                        continue
+                except:
+                    pass
+                try:
+                    language = await get_lang(chat_id)
+                    _ = get_string(language)
+                except:
+                    _ = get_string("en")
+                try:
+                    mystic = playing[0]["mystic"]
+                    markup = playing[0]["markup"]
+                except:
+                    continue
+                try:
+                    check = wrong[chat_id][mystic.id]
+                    if check is False:
+                        continue
+                except:
+                    pass
+                try:
+                    language = await get_lang(chat_id)
+                    _ = get_string(language)
+                except:
+                    _ = get_string("en")
                 try:
                     mystic = playing[0]["mystic"]
                     markup = playing[0]["markup"]
@@ -404,7 +426,7 @@ async def markup_timer():
                             playing[0]["dur"],
                         )
                         if markup == "stream"
-                        else telegram_markup_timer(
+                        else stream_markup_timer2(
                             _,
                             chat_id,
                             seconds_to_min(playing[0]["played"]),
