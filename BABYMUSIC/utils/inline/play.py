fc3bccf0784a0c00c0bc69bcd39270eba48713cc
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-10-10 18:08:03 +0530

    Update play.py

--- BABYMUSIC/utils/inline/play.py
+++ BABYMUSIC/utils/inline/play.py
@@ -2,7 +2,7 @@ import math
 
 from pyrogram.types import InlineKeyboardButton
 
-from SONALI.utils.formatters import time_to_seconds
+from BABYMUSIC.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
